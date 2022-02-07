da0b6df6b20948d4a1cc4823554a41395320a0a3
Dmitrii Sviridov <dm.sviridov99@yandex.ru>
2022-02-07 18:41:51 +0300

    change way of creating Egg6

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -3,7 +3,7 @@ package koschei.models;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
+
 public class Egg6 {
     private Needle7 needle;
     @Autowired
